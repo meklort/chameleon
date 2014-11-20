b6f4aacbc34a7dc31aa591c4114eaf247808834d
meklort <meklort>
2014-11-20 05:04:48 +0000

    New module to house main chameleon code.

---
+++ i386/modules/Chameleon/module.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 Evan Lojewski. All rights reserved.
+ *
+ *
+ * Original code moved from libsaio/spd.[c,h]
+ */
+
+#include <modules.h>
+#include <pci.h>
+#include <boot.h>
+#include <bootstruct.h>
+
+#include "hpet.h"
+
+static void found_pci_device(void* device, void* arg1, void* arg2, void* arg3);
+
+void HPETEnabler_init()
+{
+    register_hook_callback("PCIDevice", &found_pci_device);
+}
+
+static void found_pci_device(void* device, void* arg1, void* arg2, void* arg3)
+{
+    bool do_enable_hpet = false;
+    pci_dt_t *current = device;
+
+    
+    if(current->class_id == PCI_CLASS_BRIDGE_ISA)
+    {
+        getBoolForKey(kForceHPET, &do_enable_hpet, &bootInfo->chameleonConfig);
+
+        if (do_enable_hpet)
+        {
+            force_enable_hpet(current);
+        }
+    }
+}
