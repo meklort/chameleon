9cddc80b0e0b728c1d9ab42e3bfa2800031491d1
Evan Lojewski <github@meklort.com>
2017-04-16 00:18:29 -0600

    update shutdown method to support newer qemu.

--- i386/libsaio/shutdown.c
+++ i386/libsaio/shutdown.c
@@ -3,22 +3,25 @@
 
 void system_shutdown (void)
 {
-  // Notify clients that we are abuit to exit.
-  execute_hook("Exit", NULL, NULL, NULL, NULL);
+    // Notify clients that we are abuit to exit.
+    execute_hook("Exit", NULL, NULL, NULL, NULL);
 
-  asm volatile ("cli");
-  for (;;)
+    asm volatile ("cli");
+    for (;;)
     {
-      // (phony) ACPI shutdown (http://forum.osdev.org/viewtopic.php?t=16990)
-      // Works for qemu and bochs.
-      outw (0xB004, 0x2000);
+        // (phony) ACPI shutdown (http://forum.osdev.org/viewtopic.php?t=16990)
+        // Works for qemu and bochs.
+        outw (0xB004, 0x2000);
 
-      // Magic shutdown code for bochs and qemu.
-      for (const char *s = "Shutdown"; *s; ++s)
-        outb (0x8900, *s);
+        // isa shutdown for qemu (newer)
+        outb(0xf4, 0x00);
 
-      // Magic code for VMWare. Also a hard lock.
-      asm volatile ("cli; hlt");
+        // Magic shutdown code for bochs and qemu.
+        for (const char *s = "Shutdown"; *s; ++s)
+            outb (0x8900, *s);
+
+        // Magic code for VMWare. Also a hard lock.
+        asm volatile ("cli; hlt");
     }
 }
 
